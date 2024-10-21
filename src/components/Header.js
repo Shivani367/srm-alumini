56ee7179a13874b277f033c53a1750dc3a1f21e6
Shivani <122731536+Shivani367@users.noreply.github.com>
2024-10-21 10:12:55 +0530

    Update Header.js
    logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
         <div className="flex items-center space-x-4">
           <Link to="/">
           <img 
-            src="https://srmeaswari.ac.in/wp-content/uploads/2022/06/eec-logo-finalized-300x101.png" 
+            src="https://in.images.search.yahoo.com/search/images;_ylt=AwrKCT2b2hVnRAIAAtu7HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3Nj?type=E210IN1494G0&p=easwari+engineering+college+logo&fr=mcafee&th=300&tw=300&imgurl=https%3A%2F%2Fin.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Deaswari%2Bengineering%2Bcollege%2Blogo&rurl=https%3A%2F%2Fin.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Deaswari%2Bengineering%2Bcollege%2Blogo&name=Easwari+Engineering+College&h=322&w=322&turl=https%3A%2F%2Fs.yimg.com%2Fzb%2Fimgv1%2Fe7ffb84c-b0a5-3d3f-8311-7fcecbcff60c%2Ft_500x300&tt=Easwari+Engineering+College&sigr=Ut9cavxSGjYa&sigit=oZXb0.q4HOAT&sigi=jIoyJryAHTdi&sign=XggfIMPy9DjZ&sigt=XggfIMPy9DjZ" 
             alt="Easwari Engineering College Logo" 
             className="w-12 h-12"
           />
