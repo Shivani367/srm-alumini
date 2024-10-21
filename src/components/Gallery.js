acbc533944e0618affa433cbeec8dbf72c45dd98
Shivani <122731536+Shivani367@users.noreply.github.com>
2024-10-21 09:07:52 +0530

    Update Gallery.js
    gallery changes

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Gallery = () => {
   const images = [
-    'https://images.unsplash.com/photo-1498830923224-e1c12d1564a6?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',  // Placeholder images; replace with actual URLs
-    'https://media.istockphoto.com/id/916430298/photo/multi-ethnic-college-students-discussing-project-together.jpg?s=1024x1024&w=is&k=20&c=0It4wrNwIcy_HmwAX63X_ncYDB9xfJsfCuqsyifGHfg=',
-    'https://images.unsplash.com/photo-1620989983305-be972a0e290e?q=80&w=2453&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1648183185045-7a5592e66e9c?q=80&w=2488&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://srmeaswari.ac.in/wp-content/uploads/2023/08/1-1.jpg',  // Placeholder images; replace with actual URLs
+    'C:\Users\mvenk\OneDrive\Desktop\galleryalumni\thanu.jpeg',
+    'https://universitykart.b-cdn.net/Content/upload/admin/kq1ky0s4.ibt.jpg',
+    'https://srmeaswari.ac.in/wp-content/uploads/2023/08/3.jpg',
     'https://plus.unsplash.com/premium_photo-1723489348868-aa41e1ecac89?q=80&w=2878&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     'https://images.unsplash.com/photo-1551731409-43eb3e517a1a?q=80&w=2944&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   ];
